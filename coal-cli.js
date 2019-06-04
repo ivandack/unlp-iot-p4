ad661e02acec738738e6281cd58a50d4694ab7fc
Ivan Dackiewicz <ivandack@gmail.com>
2019-06-04 00:44:07 -0300

    Agregado manejo de path

--- coal-cli.js
+++ coal-cli.js
@@ -3,8 +3,9 @@ const params = require('./param-parser');
 
 const coapUrl = `coap://${params.host}:${params.port}`;
 if (params.verbose) console.log(`Host destino: ${coapUrl}`);
+if (params.verbose && params.path) console.log(`Path: ${params.path}`);
 
-const req = coap.request(coapUrl);
+const req = coap.request(`${coapUrl}${params.path}`);
 
 req.on('response', function(res) {
   res.pipe(process.stdout);
