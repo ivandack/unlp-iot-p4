0740fa4dc424617dfae63892bdda88f12b8c2ada
Ivan Dackiewicz <ivandack@gmail.com>
2019-06-04 21:21:43 -0300

    Simplificacion

--- coap-cli.js
+++ coap-cli.js
@@ -1,4 +1,17 @@
-const params = require('./param-parser');
+const commander = require('commander');
+const coap = require('./coap-lib');
+const utils = require('./utils');
 
-params.parseMain();
-process.exit(0);
+commander
+  .command('get <host> <path>')
+  .description('ejecuta el método GET en el path indicado')
+  .option('-p, --port <number>', 'Puerto del servidor CoAP (por defecto 5683)', utils.parsePort, 5683)
+  .option('-v, --verbose', 'Muestra más información al ejecutar el programa', () => 1, 0)
+  .action(function (host, path, options) {
+    const port = options.port;
+    path = path.startsWith('/') ? path : '/' + path;
+    if (options.verbose) console.log(`Host destino: "coap://${host}:${port}${path}"`);
+    coap.sendReq(host, port, path, 'get', options.verbose);
+  });
+
+commander.parse(process.argv);
--- coap-lib.js
+++ coap-lib.js
@@ -1,7 +1,7 @@
 const coap = require('coap');
 const utils = require('./utils');
 
-function sendReq(host, port, path, method) {
+function sendReq(host, port, path, method, verbose = false) {
   const reqParams = {
     hostname: host,
     port: port,
@@ -9,12 +9,15 @@ function sendReq(host, port, path, method) {
     pathname: path
   };
   const req = coap.request(reqParams);
+  if (verbose) console.log(`Enviando request ${method.toUpperCase()} a coap://${host}:${port}${path}`);
   
   req.on('response', function(res) {
     if (res.status === 404) {
       utils.exitWithError('No se encontró el método especificado');
     }
-    console.log(´Respuesta: \n${res.body}´);
+    if (verbose) console.log(`Resultado exitoso\nRespuesta:`);
+    console.log(res.body);
+    res.end();
   });
 }
 
