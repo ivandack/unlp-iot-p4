fecacf1f34c1f497d015981e16dc5a7e26670cc9
Ivan Dackiewicz <ivandack@gmail.com>
2019-06-05 02:57:45 +0200

    App funcionando

--- coap-lib.js
+++ coap-lib.js
@@ -1,5 +1,10 @@
+const fs = require('fs');
 const coap = require('coap');
 const utils = require('./utils');
+
+function getStatusCode(res) {
+  return parseInt(res.code.replace('.', ''));
+}
 
 function sendReq(host, port, path, method, verbose = false) {
   const reqParams = {
@@ -10,15 +15,18 @@ function sendReq(host, port, path, method, verbose = false) {
   };
   const req = coap.request(reqParams);
   if (verbose) console.log(`Enviando request ${method.toUpperCase()} a coap://${host}:${port}${path}`);
-  
-  req.on('response', function(res) {
-    if (res.status === 404) {
+
+  req.on('response', (res) => {
+    if (getStatusCode(res) === 404) {
       utils.exitWithError('No se encontró el método especificado');
     }
     if (verbose) console.log(`Resultado exitoso\nRespuesta:`);
-    console.log(res.body);
-    res.end();
+    res.pipe(process.stdout);
+    res.on('end', () => {
+      console.log();
+    });
   });
+  req.end();
 }
 
 module.exports = {
